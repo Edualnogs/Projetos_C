1adb03507bd90d857d1609b77565b862be5792bd
Eduardo <136937070+Edualnogs@users.noreply.github.com>
2023-08-15 20:07:39 -0300

    Delete Programas01 directory

--- Programas01/prog001.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-#define pi 3.1415
-
-int main(){
-    printf("A constante pi é %f\n", pi);
-}
--- Programas01/soma001.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int n1 = 5, n2 = 2;
-    printf("A soma entre %d e %d é %d\n", n1, n2, n1+n2);
-}
